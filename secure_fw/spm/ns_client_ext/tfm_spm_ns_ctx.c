5713754471abe0e08123bcaf06676537e0a4d945
shyjun <snarangaprath@innophaseiot.com>
2025-07-31 15:32:23 +0530

    enable back tfm_nspm_ctx_init

--- secure_fw/spm/ns_client_ext/tfm_spm_ns_ctx.c
+++ secure_fw/spm/ns_client_ext/tfm_spm_ns_ctx.c
@@ -90,8 +90,6 @@ int32_t tfm_nspm_get_current_client_id(void)
 
 void tfm_nspm_ctx_init(void)
 {
-//MHK
-#if 0
 #ifdef TFM_PARTITION_NS_AGENT_TZ
     /* SCB_NS.VTOR points to the Non-secure vector table base address */
     SCB_NS->VTOR = tfm_hal_get_ns_VTOR();
@@ -105,5 +103,4 @@ void tfm_nspm_ctx_init(void)
         tfm_core_panic();
     }
 #endif
-#endif //MHK    
 }
