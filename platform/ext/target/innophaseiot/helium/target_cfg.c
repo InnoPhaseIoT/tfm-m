b9ea02d2f537e3da006de7143601677dd15905b5
shyjun <snarangaprath@innophaseiot.com>
2025-08-06 09:23:42 +0530

    nsc is moved to 0x300e0000, 1 bank fully given. optimize later

--- platform/ext/target/innophaseiot/helium/target_cfg.c
+++ platform/ext/target/innophaseiot/helium/target_cfg.c
@@ -391,8 +391,8 @@ const struct sau_cfg_t sau_cfg[] = {
     },
 #endif
 	{
-        (uint32_t)&REGION_NAME(Image$$, ER_VENEER, $$Base),
-        (uint32_t)&REGION_NAME(Image$$, VENEER_ALIGN, $$Limit) - 1,
+        0x300E0000,
+        0x300EFFFF,
         true,
     },
     {
