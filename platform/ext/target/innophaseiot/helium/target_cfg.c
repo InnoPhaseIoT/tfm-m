a42feeccb857523797e974c0564026474315f0fd
shyjun <snarangaprath@innophaseiot.com>
2025-08-22 16:19:02 +0530

    updated the ppc numbers

--- platform/ext/target/innophaseiot/helium/target_cfg.c
+++ platform/ext/target/innophaseiot/helium/target_cfg.c
@@ -870,7 +870,7 @@ FIH_RET_TYPE(int32_t) ppc_init_cfg(void)
 
     // System Management Unit
     periph = 0;
-    periph |= (1<<SPCNTL_SYS_SMU_POS);  // assign SYSTEM_MANAGEMENT_UNIT to ns
+    periph |= (1<<SPCNTL_APB_SYS_SMU_POS);  // assign SYSTEM_MANAGEMENT_UNIT to ns
     err |= ppc_devices_set_to_ns(&Driver_APB_SYSCNTRL, periph);
     assert(err == 0);
 
@@ -891,9 +891,9 @@ FIH_RET_TYPE(int32_t) ppc_init_cfg(void)
 
     // APB_SYS PPC initialize
     periph = 0;
-    periph |= (1<<SPCNTL_SYS_RTC_POS);  // assign Real Time Counter (cast) to ns
-    periph |= (1<<SPCNTL_SYS_LPT_POS);  // assign Low Power Timer (cast wdt) to ns
-    periph |= (1<<SPCNTL_SYS_WDT_POS);  // assign Wachdog timer (cast) to ns
+    periph |= (1<<SPCNTL_APB_SYS_RTC_POS);  // assign Real Time Counter (cast) to ns
+    periph |= (1<<SPCNTL_APB_SYS_LPT_POS);  // assign Low Power Timer (cast wdt) to ns
+    periph |= (1<<SPCNTL_APB_SYS_WDT_POS);  // assign Wachdog timer (cast) to ns
     err |= ppc_devices_set_to_ns(&Driver_APB_SYSCNTRL, periph);
     assert(err == 0);
 
