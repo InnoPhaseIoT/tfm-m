5ece7b1439cbee5f5dce01112836981baa6f8722
shyjun <snarangaprath@innophaseiot.com>
2025-08-26 16:27:26 +0530

    dont give rtc to non-secure. hw bug causes crash

--- platform/ext/target/innophaseiot/helium/target_cfg.c
+++ platform/ext/target/innophaseiot/helium/target_cfg.c
@@ -895,7 +895,7 @@ FIH_RET_TYPE(int32_t) ppc_init_cfg(void)
 
     // APB_SYS PPC initialize
     periph = 0;
-    periph |= (1<<SPCNTL_APB_SYS_RTC_POS);  // assign Real Time Counter (cast) to ns
+    //periph |= (1<<SPCNTL_APB_SYS_RTC_POS);  // assign Real Time Counter (cast) to ns
     periph |= (1<<SPCNTL_APB_SYS_LPT_POS);  // assign Low Power Timer (cast wdt) to ns
     periph |= (1<<SPCNTL_APB_SYS_WDT_POS);  // assign Wachdog timer (cast) to ns
     err |= ppc_devices_set_to_ns(&Driver_APB_SYSCNTRL, periph);
