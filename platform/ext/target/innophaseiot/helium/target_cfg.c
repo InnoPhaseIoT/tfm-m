a019777725e0680dd23d4815b568c60846604960
Maulik Jodhani <mjodhani@innophaseiot.com>
2025-08-28 16:41:27 +0530

    i2c_0, i2c_1 given to ns

--- platform/ext/target/innophaseiot/helium/target_cfg.c
+++ platform/ext/target/innophaseiot/helium/target_cfg.c
@@ -880,7 +880,9 @@ FIH_RET_TYPE(int32_t) ppc_init_cfg(void)
     periph |= (1<<SPCNTL_PERIPH0_UART1_POS);  // assign UART_1 to ns
     periph |= (1<<SPCNTL_PERIPH0_UART2_POS);  // assign UART_2 to ns
     periph |= (1<<SPCNTL_PERIPH0_SPI0_POS);  // assign SPI_0 to ns
-    periph |= (1<<SPCNTL_PERIPH0_SPI1_POS);  // assign SPI_0 to ns
+    periph |= (1<<SPCNTL_PERIPH0_SPI1_POS);  // assign SPI_1 to ns
+    periph |= (1<<SPCNTL_PERIPH0_I2C0_POS);  // assign I2C_0 to ns
+    periph |= (1<<SPCNTL_PERIPH0_I2C1_POS);  // assign I2C_1 to ns
     err |= ppc_devices_set_to_ns(&Driver_AHB_PERIPHERAL0, periph);
     assert(err == 0);
 
